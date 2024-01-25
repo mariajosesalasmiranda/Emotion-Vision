d071055a62725c9943706b3a4d142e166cce0b9a
Maria José Salas Miranda <mariajose.salasmiranda@gmail.com>
2024-01-25 03:33:54 +0100

    update

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -35,3 +35,36 @@ if not cap.isOpened():
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
+while True:
+    ret, frame = cap.read()
+    faceCascade = cv2.cascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+    for x,y,w,h in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        cv2.rectangle(frame,(x,y),(x+w,y+h), (255,0,0),2)
+        facess = faceCascade.detectMultiScale(roi_gray)
+        if len(faces) == 0:
+            print("Face not detected")
+        else:
+            for (ex,ey,ew,eh) in facess:
+                face_roi = roi_color[ey:ey+eh,ex:ex+ew]
+
+    final_image = cv2.resize(face_roi,(224,224))
+    final_image = np.expand_dims(final_image, axis=0) # need fourth dimension
+    final_image = final_image/255.0 
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    Predictions = new_model.predict(final_image)
+
+    font_scale = 1.5
+    font = cv2.FONT_HERSHEY_PLAIN
+
+    if (np.argmax(Predictions) == 0):
+        status = "Angry"
+
+        x1,y1,w1,h1 = 0,0,175,75
+        cv2.rectangle(frame,(x1,x1), (x1+w1,y1+h1), (0,0,0), -1)
+        cv2.putText(frame)
